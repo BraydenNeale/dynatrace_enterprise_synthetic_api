7269ad07a5f8900cd33e9b6c1ec7544350abafd6
Brayden Neale <brayden.neale@gmail.com>
2018-07-22 16:01:44 +1000

    Read config from ini file, request data from DCRUM API

--- ES_script.py
+++ ES_script.py
@@ -1,64 +1,26 @@
-"""
-Example script that shows how to send external synthetic data into Dynatrace.
-"""
-import requests, time
+import requests, time, configparser
 
-YOUR_DT_API_URL = '<DT_API>'
-YOUR_DT_API_TOKEN = '<TOKEN>';
-DCRUM_QUERY_URL = "<DCRUM_URL>/rest/dmiquery/getDMIData3?appId=CVENT&viewId=TransAct&dimensionIds=['bgTrans','transTimestamp']&metricIds=['Avb','transTime']&resolution=r&dimFilters=[]&metricFilters=[]&sort=[['transTimestamp',DESC]]&topFilter=1000&timePeriod=1H&numberOfPeriods=1&dataSourceId=ALL_AGGR"
+config = configparser.ConfigParser()
+config_path = './config.ini'
+config.read(config_path)
 
-syntheticData = requests.get(DCRUM_QUERY_URL);
-responseTime = syntheticRequest.elapsed.total_seconds();
+DT_API_URL = config.get('dynatrace','DT_API_URL')
+DT_API_TOKEN = config.get('dynatrace', 'DT_API_TOKEN')
+DCRUM_QUERY_URL = config.get('dcrum', 'DCRUM_QUERY_URL')
+DCRUM_USER = config.get('dcrum', 'DCRUM_USER')
+DCRUM_PWD = config.get('dcrum', 'DCRUM_PWD')
 
-success = False;
-if (syntheticRequest.status_code == requests.codes.ok): 
-    success = True;
-	
-print ('Synthetic request - Response time:' + str(responseTime) + 's Status code:' + str(syntheticRequest.status_code) + " Success:" + str(success));
+r = requests.get(DCRUM_QUERY_URL, auth=(DCRUM_USER, DCRUM_PWD))
 
-timestamp = time.time() * 1000;
+if (not r.status_code == requests.codes.ok):
+    exit()
 
-payload = {
-    "messageTimestamp": timestamp,
-    "syntheticEngineName": "My test",
-    "syntheticEngineIconUrl": "http://assets.dynatrace.com/global/icons/cpu_processor.png",
-    "locations": [{
-         "id": "1",
-         "name": "Linz"
-    }],
-    "tests" : [ {
-        "id": "1",
-        "title": "Example.com",
-        "testSetup":  "Python script",
-        "drilldownLink": "http://example.com/",
-        "enabled": True,
-        "locations": [ {
-            "id": "1",
-            "enabled": True
-        }],
-        "steps": [ {
-            "id": 1,
-            "title": "Get request example.com"
-        }]
-    }],    
-    "testResults": [{
-        "id": "1",
-        "scheduleIntervalInSeconds": 60,
-        "totalStepCount": 1,
-        "locationResults": [{
-            "id": "1",
-            "startTimestamp": timestamp,
-            "success": success,
-            "stepResults": [{
-                "id" : 1,
-                "startTimestamp": timestamp,
-                "responseTimeMillis": responseTime * 1000
-            }]
-    }]
-}]};
+print(r.text)
 
+'''
 headers = {'content-type': 'application/json'};
-r = requests.post(YOUR_DT_API_URL + '/api/v1/synthetic/ext/tests?Api-Token=' + YOUR_DT_API_TOKEN, json=payload, headers=headers);
+r = requests.post(DT_API_URL + '/api/v1/synthetic/ext/tests?Api-Token=' + DT_API_TOKEN, json=payload, headers=headers);
 # Output API response 
 print(r);
 print(r.text);
+'''
