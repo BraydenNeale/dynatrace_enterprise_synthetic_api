6951c1811d047525738196670372cb40b32a723f
Brayden Neale <brayden.neale@gmail.com>
2018-07-30 20:33:57 +1000

    Add each test into a dict + example config

--- ES_script.py
+++ ES_script.py
@@ -1,4 +1,4 @@
-import requests, time, configparser
+import requests, time, configparser, json
 
 config = configparser.ConfigParser()
 config_path = './config.ini'
@@ -15,7 +15,26 @@ r = requests.get(DCRUM_QUERY_URL, auth=(DCRUM_USER, DCRUM_PWD))
 if (not r.status_code == requests.codes.ok):
     exit()
 
-print(r.text)
+#test_data = r.json()['formattedData']
+with open('example_payload.json') as f:
+    test_data = json.load(f)['formattedData']
+
+# TODO - group like steps into a single test
+# currently just treating all steps as invidual tests
+steps = []
+for s in test_data:
+	step = {}
+	step['name'] = s[0]
+	step['start_time'] = s[1]
+	step['pass'] = True if s[2] == '100.0' else False
+	step['response_time'] = s[3]
+	steps.append(step)
+
+for step in steps:
+	print('\n')
+	for k,v in step.items():
+		print(v)
+		# Add to request
 
 '''
 headers = {'content-type': 'application/json'};
